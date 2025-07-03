a7d258a23f2ea7cb0d2cefdce05c0e1f20c0fa08
Armando Lazarte <armandolazarte@users.noreply.github.com>
2025-07-03 20:11:44 -0300

    ✨ feat(padron): agregar servicio padron alcance diez
    - Se agrega la clase PadronAlcanceDiez para interactuar con el webservice de AFIP
    - Permite consultar datos de contribuyentes a partir de su CUIT
    - Implementa métodos para obtener datos del contribuyente y estado del servidor
    - Utiliza token de autorización para la seguridad de las consultas

---
+++ src/WebService/PadronAlcanceDiez.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpAfipWs\WebService;
+
+use PhpAfipWs\Exception\AfipException;
+
+/**
+ * Clase para interactuar con el Web Service de Padrón Alcance 10 de AFIP (ws_sr_padron_a10).
+ *
+ * Permite consultar los datos de un contribuyente a partir de su CUIT.
+ */
+class PadronAlcanceDiez extends AfipWebService
+{
+    /** {@inheritdoc} */
+    protected ?string $nombreWsdl = 'ws_sr_padron_a10-production.wsdl';
+
+    /** {@inheritdoc} */
+    protected ?string $urlProduccion = 'https://aws.afip.gov.ar/sr-padron/webservices/personaServiceA10';
+
+    /** {@inheritdoc} */
+    protected ?string $nombreWsdlPrueba = 'ws_sr_padron_a10.wsdl';
+
+    /** {@inheritdoc} */
+    protected ?string $urlPrueba = 'https://awshomo.afip.gov.ar/sr-padron/webservices/personaServiceA10';
+
+    /**
+     * Consulta y obtiene los datos de un contribuyente registrados en AFIP.
+     *
+     * @param  int  $identificador  El CUIT del contribuyente a consultar.
+     * @return mixed La respuesta del servidor con los datos del contribuyente.
+     *
+     * @throws AfipException Si ocurre un error al obtener el TA o en la comunicación SOAP.
+     */
+    public function getDatosContribuyente(int $identificador): mixed
+    {
+        $tokenAutorizacion = $this->getTokenAutorizacion();
+
+        $parametros = [
+            'token' => $tokenAutorizacion->getToken(),
+            'sign' => $tokenAutorizacion->getSign(),
+            'cuitRepresentada' => $this->afip->getCuit(),
+            'idPersona' => $identificador,
+        ];
+
+        return $this->ejecutar('getPersona', $parametros);
+    }
+
+    /**
+     * Consulta el estado de los servidores de AFIP para este servicio.
+     *
+     * @return mixed La respuesta del servidor con el estado.
+     *
+     * @throws AfipException Si ocurre un error en la comunicación SOAP.
+     */
+    public function getEstadoServidor(): mixed
+    {
+        return $this->ejecutar('dummy');
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * Devuelve el nombre del servicio para la autorización del token.
+     */
+    protected function getNombreServicio(): string
+    {
+        return 'ws_sr_padron_a10';
+    }
+}
